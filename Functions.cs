2b1716e2be94e261ec4483a2dbe17ea24ca42d70
waitskoro <cristina.guryanova2017@yandex.ru>
2023-05-18 23:53:19 +0700

    Add functions

---
+++ Functions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static partial class Program
+{
+    static void Division(double a, double b)
+    {
+        double total = a / b;
+        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+    }
+
+    static void Multiplication(double a, double b)
+    {
+        double total = a * b;
+        Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+    }
+
+    static void Subtraction(double a, double b)
+    {
+        double total = a - b;
+        Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+    }
+
+    static void Sum(double a, double b)
+    {
+        double total = a + b;
+        Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,9 +1,44 @@
-﻿public class Program
+﻿static partial class Program
 {
 
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
 
-        Console.WriteLine("Hello, World!");
+        double a;
+        double b;
+        char oper;
+
+        Console.WriteLine("Введите первое число:");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите оператор:");
+        oper = Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        if (oper == '+')
+        {
+            Sum(a, b);
+        }
+
+        else if (oper == '-')
+        {
+            Subtraction(a, b);
+        }
+
+        else if (oper == '*')
+        {
+            Multiplication(a, b);
+        }
+
+        else if (oper == '/')
+        {
+            Division(a, b);
+        }
+        else
+        {
+            Console.WriteLine("Неизвестный оператор.");
+        }
     }
 }
